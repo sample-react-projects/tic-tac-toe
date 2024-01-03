ff11a0ce75fbe82e959bfce83781c3340b5d97d9
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-03 16:13:51 +0530

    Create modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,25 @@
+import styles from "./Modal.module.scss";
+
+interface IModal {
+  message: string;
+  actionLabel: string;
+  handleActionClick: () => void;
+}
+const Modal: React.FC<IModal> = ({
+  actionLabel,
+  handleActionClick,
+  message,
+}) => {
+  return (
+    <div className={styles.modal__mask}>
+      <div className={styles.modal__content}>
+        <div className={styles.modal__text}>{message}</div>
+        <button className="primary" onClick={handleActionClick}>
+          {actionLabel}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
