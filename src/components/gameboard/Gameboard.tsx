9d9751210ef2e1390024851908bfe1a8dadb2d7c
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-04 16:59:16 +0530

    eliminate extra props from gameboard

--- src/components/gameboard/Gameboard.tsx
+++ src/components/gameboard/Gameboard.tsx
@@ -1,21 +1,11 @@
 import styles from "./Gameboard.module.scss";
-import { BoardSteps, PlayerSymbol } from "../app/App";
 
 interface IGameboard {
-  activePlayer: PlayerSymbol;
   board: string[][];
-  setSteps: React.Dispatch<React.SetStateAction<BoardSteps[]>>;
+  updateSteps: (row: number, col: number) => void;
 }
 
-const Gameboard: React.FC<IGameboard> = ({
-  activePlayer,
-  board,
-  setSteps,
-}) => {
-  function handleClick(row: number, col: number) {
-    setSteps((prevSteps) => [...prevSteps, { row, col, activePlayer }]);
-  }
-
+const Gameboard: React.FC<IGameboard> = ({ board, updateSteps }) => {
   return (
     <div className={styles.gameboard}>
       <div className={styles.gameboard__cells}>
@@ -25,7 +15,7 @@ const Gameboard: React.FC<IGameboard> = ({
               key={`${rowIndex}-${colIndex}`}
               className={styles.gameboard__cell}
               disabled={!!symbol}
-              onClick={() => handleClick(rowIndex, colIndex)}
+              onClick={() => updateSteps(rowIndex, colIndex)}
             >
               {symbol ? (
                 <span className={styles.gameboard__symbol}>{symbol}</span>
