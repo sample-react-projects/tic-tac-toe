01fe84a1fa1a6681f449253329a4f9364f50c5c8
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-04 10:26:09 +0530

    Use updated card component

--- src/components/players/Players.tsx
+++ src/components/players/Players.tsx
@@ -1,25 +1,29 @@
 import { PlayerSymbol } from "../app/App";
 import Player from "../player/Player";
+import Card from "../util/card/Card";
 import styles from "./Players.module.scss";
 
 interface IPlayers {
+  activePlayer: PlayerSymbol;
   players: Record<PlayerSymbol, string>;
   setPlayers: React.Dispatch<
     React.SetStateAction<Record<PlayerSymbol, string>>
   >;
 }
 
-const Players: React.FC<IPlayers> = ({ players, setPlayers }) => {
+const Players: React.FC<IPlayers> = ({ activePlayer, players, setPlayers }) => {
   return (
     <div className={styles.players}>
       {(Object.entries(players) as [PlayerSymbol, string][]).map(
         ([symbol, name]) => (
-          <Player
-            key={symbol}
-            symbol={symbol}
-            name={name}
-            setPlayers={setPlayers}
-          ></Player>
+          <Card active={activePlayer === symbol}>
+            <Player
+              key={symbol}
+              symbol={symbol}
+              name={name}
+              setPlayers={setPlayers}
+            ></Player>
+          </Card>
         )
       )}
     </div>
